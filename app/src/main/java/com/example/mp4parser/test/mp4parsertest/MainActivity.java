c595b5967efb33edcdb101b1bfc798cc8835383e
Jonathan Girven <jonathan.girven@trickbook.com>
2015-01-06 18:12:22 +0000

    Initial commit

---
+++ app/src/main/java/com/example/mp4parser/test/mp4parsertest/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.mp4parser.test.mp4parsertest;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Button shortenButton;
+    private EditText startET;
+    private EditText endET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        shortenButton = (Button) findViewById(R.id.shorten_file_button);
+        shortenButton.setOnClickListener(this);
+
+        startET = (EditText) findViewById(R.id.start_edit_text);
+        endET = (EditText) findViewById(R.id.end_edit_text);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Log.d(TAG, "onClick");
+
+        if ((startET.getText().toString().equals("")) || (endET.getText().toString().equals(""))) {
+            Log.d(TAG, "onClick: Empty");
+            return;
+        }
+
+        int startMS = Integer.parseInt(startET.getText().toString());
+        int endMS = Integer.parseInt(endET.getText().toString());
+
+        Log.d(TAG, "onClick: startMS: " + startMS + ", endMS: " + endMS);
+
+        shortenButton.setEnabled(false);
+        new ShortenAsync(startMS, endMS).execute();
+    }
+
+
+    private class ShortenAsync extends AsyncTask<Void, Void, Void> {
+
+        private final int startMs;
+        private final int endMs;
+
+        public ShortenAsync(int startMs, int endMs) {
+            this.startMs = startMs;
+            this.endMs = endMs;
+        }
+
+        @Override
+        protected Void doInBackground(Void... nothing) {
+            File moviesDir = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_MOVIES
+            );
+
+            String filePrefix = "GOPR1008";
+            String fileExtn = ".MP4";
+            String fileName = filePrefix + fileExtn;
+
+            try {
+                InputStream inputStream = getAssets().open(fileName);
+                File src = new File(moviesDir, fileName);
+
+                storeFile(inputStream, src);
+
+                File dest = new File(moviesDir, filePrefix + "_1" + fileExtn);
+
+                Log.d(TAG, "startTrim: src: " + src.getAbsolutePath());
+                Log.d(TAG, "startTrim: dest: " + dest.getAbsolutePath());
+                Log.d(TAG, "startTrim: startMs: " + startMs);
+                Log.d(TAG, "startTrim: endMs: " + endMs);
+                ShortenExample.startTrim(src, dest, startMs, endMs);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void nothing) {
+            Log.d(TAG, "onPostExecute");
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(MainActivity.this, R.string.complete, Toast.LENGTH_SHORT).show();
+                    if (shortenButton != null) shortenButton.setEnabled(true);
+                }
+            });
+        }
+    }
+
+
+    private void storeFile(InputStream input, File file) {
+        try {
+            final OutputStream output = new FileOutputStream(file);
+            try {
+                try {
+                    final byte[] buffer = new byte[1024];
+                    int read;
+
+                    while ((read = input.read(buffer)) != -1)
+                        output.write(buffer, 0, read);
+
+                    output.flush();
+                } finally {
+                    output.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                input.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mp4parser/test/mp4parsertest/ShortenExample.java
@@ -0,0 +1,130 @@
+package com.example.mp4parser.test.mp4parsertest;
+
+import com.coremedia.iso.boxes.Container;
+import com.googlecode.mp4parser.FileDataSourceImpl;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.Track;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
+import com.googlecode.mp4parser.authoring.tracks.AppendTrack;
+import com.googlecode.mp4parser.authoring.tracks.CroppedTrack;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Shortens/Crops a track
+ *
+ * https://github.com/sannies/mp4parser/blob/master/examples/src/main/java/com/googlecode/mp4parser/ShortenExample.java
+ * Slightly modified to take arguments
+ */
+public class ShortenExample {
+
+    public static void startTrim(File src, File dst, int startMs, int endMs) throws IOException {
+        Movie movie = MovieCreator.build(new FileDataSourceImpl(src));
+
+        List<Track> tracks = movie.getTracks();
+        movie.setTracks(new LinkedList<Track>());
+        // remove all tracks we will create new tracks from the old
+
+        double startTime = startMs/1000;
+        double endTime = endMs/1000;
+
+        boolean timeCorrected = false;
+
+        // Here we try to find a track that has sync samples. Since we can only start decoding
+        // at such a sample we SHOULD make sure that the start of the new fragment is exactly
+        // such a frame
+        for (Track track : tracks) {
+            if (track.getSyncSamples() != null && track.getSyncSamples().length > 0) {
+                if (timeCorrected) {
+                    // This exception here could be a false positive in case we have multiple tracks
+                    // with sync samples at exactly the same positions. E.g. a single movie containing
+                    // multiple qualities of the same video (Microsoft Smooth Streaming file)
+
+                    throw new RuntimeException("The startTime has already been corrected by another track with SyncSample. Not Supported.");
+                }
+                startTime = correctTimeToSyncSample(track, startTime, false);
+                endTime = correctTimeToSyncSample(track, endTime, true);
+                timeCorrected = true;
+            }
+        }
+
+        for (Track track : tracks) {
+            long currentSample = 0;
+            double currentTime = 0;
+            double lastTime = -1;
+            long startSample = -1;
+            long endSample = -1;
+
+            for (int i = 0; i < track.getSampleDurations().length; i++) {
+                long delta = track.getSampleDurations()[i];
+
+
+                if (currentTime > lastTime && currentTime <= startTime) {
+                    // current sample is still before the new starttime
+                    startSample = currentSample;
+                }
+                if (currentTime > lastTime && currentTime <= endTime) {
+                    // current sample is after the new start time and still before the new endtime
+                    endSample = currentSample;
+                }
+                lastTime = currentTime;
+                currentTime += (double) delta / (double) track.getTrackMetaData().getTimescale();
+                currentSample++;
+            }
+            movie.addTrack(new CroppedTrack(track, startSample, endSample));
+        }
+
+
+        Container out = new DefaultMp4Builder().build(movie);
+
+        if (!dst.exists()) {
+            dst.createNewFile();
+        }
+
+        FileOutputStream fos = new FileOutputStream(dst);
+        FileChannel fc = fos.getChannel();
+        out.writeContainer(fc);
+
+        fc.close();
+        fos.close();
+    }
+
+
+    private static double correctTimeToSyncSample(Track track, double cutHere, boolean next) {
+        double[] timeOfSyncSamples = new double[track.getSyncSamples().length];
+        long currentSample = 0;
+        double currentTime = 0;
+        for (int i = 0; i < track.getSampleDurations().length; i++) {
+            long delta = track.getSampleDurations()[i];
+
+            if (Arrays.binarySearch(track.getSyncSamples(), currentSample + 1) >= 0) {
+                // samples always start with 1 but we start with zero therefore +1
+                timeOfSyncSamples[Arrays.binarySearch(track.getSyncSamples(), currentSample + 1)] = currentTime;
+            }
+            currentTime += (double) delta / (double) track.getTrackMetaData().getTimescale();
+            currentSample++;
+
+        }
+        double previous = 0;
+        for (double timeOfSyncSample : timeOfSyncSamples) {
+            if (timeOfSyncSample > cutHere) {
+                if (next) {
+                    return timeOfSyncSample;
+                } else {
+                    return previous;
+                }
+            }
+            previous = timeOfSyncSample;
+        }
+        return timeOfSyncSamples[timeOfSyncSamples.length - 1];
+    }
+
+
+}
